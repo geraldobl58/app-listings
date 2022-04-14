c24c4a34709a81038edea6b25de995b828ea0800
Geraldo Luiz <geraldobl58@gmail.com>
2022-04-14 10:58:00 -0300

    :sparkles: Listing ads

--- src/controllers/AdsController.js
+++ src/controllers/AdsController.js
@@ -4,6 +4,7 @@ const jimp = require('jimp');
 const Category = require('../models/Category')
 const User = require('../models/User')
 const Ad = require('../models/Ad')
+const StateModel = require('../models/State')
 
 const addImage = async (buffer) => {
   let newName = `${uuid()}.jpg`
@@ -54,7 +55,7 @@ module.exports = {
     newAd.state = user.state
     newAd.dateCreated = new Date()
     newAd.title = title
-    newAd.cat = cat
+    newAd.category = cat
     newAd.price = price
     newAd.priceNegotiable = (priceneg == 'true') ? true : false
     newAd.description = desc
@@ -92,7 +93,64 @@ module.exports = {
   },
 
   getList: async (req, res) => {
+    let { sort = 'asc', offset = 0, limit = 8, q, cat, state } = req.query
 
+    let filters = { status: true }
+
+    let total = 0
+
+    if (q) {
+      filters.title = {'$regex': q, '$options': 'i'}
+    }
+
+    if (cat) {
+      const c = await Category.findOne({ slug: cat }).exec()
+      
+      if (c) {
+        filters.category = c._id.toString()
+      }
+    }
+
+    if (state) {
+      const s = await StateModel.findOne({ name: state.toUpperCase() }).exec()
+
+      if (s) {
+        filters.state = s._id.toString()
+      }
+    }
+
+    const adsTotal = await Ad.find(filters).exec()
+    total = adsTotal.length
+
+    const adsData = await Ad.find(filters)
+      .sort({ dateCreated: (sort == 'desc' ? -1 : 1) })
+      .skip(parseInt(offset))
+      .limit(parseInt(limit))
+      .exec()
+
+    let ads = []
+
+    for(let i in adsData) {
+      let image
+
+      let defaultImg = adsData[i].images.find(e => e.default)
+
+      if (defaultImg) {
+        image = `${process.env.BASE}/media/${defaultImg.url}`
+      } else {
+        image = `${process.env.BASE}/media/default.jpg`
+      }
+
+      ads.push({
+        id: adsData[i]._id,
+        title: adsData[i].title,
+        price: adsData[i].price,
+        priceNegotiable: adsData[i].priceNegotiable,
+        image
+      })
+    }
+
+    res.json({ ads, total })
   },
 
   getItem: async (req, res) => {
